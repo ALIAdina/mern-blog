2778e6229e03883fe53ae86873952009af9a687f
linda <dina.lia34@yahoo.com>
2026-04-07 23:23:29 +0200

    test

--- client/src/pages/AuthorPosts.jsx
+++ client/src/pages/AuthorPosts.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import Post from "./Post";
+import Post from "../Post";
 import { API_URL } from "../config";
 
 export default function AuthorPosts() {
